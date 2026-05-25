88d27310a79329bb05889f0c3fd0c376e5aa62ff
Gizem <yurtsevenngizemm@gmail.com>
2026-05-25 17:05:23 +0300

    Update graph.h guncellendi, eksik fonksiyonlar eklendi
    Trie metin araması, yönsüz ilişki desteği ve Faz 3 Graphviz DOT görselleştirme fonksiyonlarının prototipleri graph.h dosyasına eklendi.

--- graph/graph.h
+++ graph/graph.h
@@ -2,18 +2,23 @@
 #define GRAPH_H
 
 #include "../hash/hash.h"
+#include "../trie/trie.h"
 
-void addEdge(HashTable* ht, int src, int dest, char* relation);
-
-// BFS
+// Mevcut Fonksiyonlar
+void addEdge(HashTable* ht, int src, int dest, char* relation, int isDirected); // isDirected eklendi
 void BFS(HashTable* ht, int start_id);
-
-// DFS
 void DFS(HashTable* ht, int start_id);
-
-// Triadic closure / friend recommendations
 void recommendFriends(HashTable* ht, int node_id, int minMutualFriends);
-
+int getDegreeCentrality(HashTable* ht, int node_id);
+void printNodeCentrality(HashTable* ht);
 void freeGraph(HashTable* ht);
+void filteredBFS(HashTable* ht, int start_id, const char* relation_filter);
+void findPhotosOfFriendsEvents(HashTable* ht, int start_user_id);
+void generateSyntheticData(HashTable* ht, int userCount, int eventCount, int photoCount, int edgeCount);
+
+// YENİ EKLENEN EKSİK ÖZELLİKLER (Faz 1 - Trie Entegrasyonu ve Faz 3 - Görselleştirme)
+void indexGraphWithTrie(HashTable* ht, TrieNode* trieRoot);
+void searchAndPrintNodesByPrefix(HashTable* ht, TrieNode* trieRoot, char* prefix);
+void exportGraphToDOT(HashTable* ht, const char* filename);
 
 #endif
