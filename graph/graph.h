17e9130f133df8c912d5e31daca9433fea2a4311
Gizem <yurtsevenngizemm@gmail.com>
2026-05-25 19:28:50 +0300

    Update graph.h
    düzenleme: Faz 1 filtreleme fonksiyonu ve yapı tanımları ana daldaki güncel halleriyle eşitlendi

--- graph/graph.h
+++ graph/graph.h
@@ -1,24 +1,28 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include "../models/node.h"
 #include "../hash/hash.h"
-#include "../trie/trie.h"
 
-// Mevcut Fonksiyonlar
-void addEdge(HashTable* ht, int src, int dest, char* relation, int isDirected); // isDirected eklendi
+// Graf Yapısı İçin Temel Fonksiyon Tanımlamaları
+void addEdge(HashTable* ht, int src, int dest, char* relation);
 void BFS(HashTable* ht, int start_id);
 void DFS(HashTable* ht, int start_id);
+
+// Faz 1: Filtrelenmiş BFS Fonksiyon Tanımı
+void filteredBFS(HashTable* ht, int start_id, const char* relation_filter);
+
+// Analiz ve Öneri Algoritmaları
 void recommendFriends(HashTable* ht, int node_id, int minMutualFriends);
 int getDegreeCentrality(HashTable* ht, int node_id);
 void printNodeCentrality(HashTable* ht);
-void freeGraph(HashTable* ht);
-void filteredBFS(HashTable* ht, int start_id, const char* relation_filter);
+
+// Faz 2 & Faz 3: Çok Adımlı Sorgu, Veri Üretimi ve JSON Çıktısı
 void findPhotosOfFriendsEvents(HashTable* ht, int start_user_id);
 void generateSyntheticData(HashTable* ht, int userCount, int eventCount, int photoCount, int edgeCount);
+void exportGraphToJSON(HashTable* ht, const char* filename);
 
-// YENİ EKLENEN EKSİK ÖZELLİKLER (Faz 1 - Trie Entegrasyonu ve Faz 3 - Görselleştirme)
-void indexGraphWithTrie(HashTable* ht, TrieNode* trieRoot);
-void searchAndPrintNodesByPrefix(HashTable* ht, TrieNode* trieRoot, char* prefix);
-void exportGraphToDOT(HashTable* ht, const char* filename);
+// Bellek Yönetimi
+void freeGraph(HashTable* ht);
 
-#endif
+#endif // GRAPH_H
